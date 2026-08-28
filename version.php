50c5e379fb949f063c90f00c49cfef1f6624d159
Urs Keller <me@ukeller.ch>
2026-08-28 21:39:50 +0200

    version.php: 2026082801 / v5.5.1-formasuisse-pooled.3

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2026082800;
-$plugin->release = 'v5.5.1-formasuisse-pooled.2';
+$plugin->version = 2026082801;
+$plugin->release = 'v5.5.1-formasuisse-pooled.3';
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
