b0972f5fbe693b05ee3db1b74e4d62128d589ff1
Urs Keller <urs@revault.ch>
2026-08-31 23:17:03 +0200

    version.php: 2026083101 / v5.5.1-formasuisse-pooled.5 (#28)

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_zoom';
-$plugin->version = 2026083100;
-$plugin->release = 'v5.5.1-formasuisse-pooled.4';
+$plugin->version = 2026083101;
+$plugin->release = 'v5.5.1-formasuisse-pooled.5';
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
